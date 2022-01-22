c9edd1740ef2f3606285f95239b24c6d0ce7d8d5
xangetsue <xangetsue@gmail.com>
2022-01-22 22:21:52 +0400

    updates...

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
-let guestName = prompt("what is your name or nickname? ");
-alert('welcome ' + guestName);
-
-document.getElementById('demo').innerHTML = "welcome here " + guestName;
+// let guestName = prompt("what is your name or nickname? ");
+// alert('welcome ' + guestName);
